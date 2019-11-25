39c4b0319868d8d735a4b71bc2ddc8251c23acde
waldenlan <446731905@qq.com>
2019-11-25 15:41:59 -0800

    Add the db password to the settings file.

--- dmarket/dmarket/settings.py
+++ dmarket/dmarket/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': '123456',
         'HOST': 'db',
         'PORT': 5432,
     }
