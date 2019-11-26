a512ef8848f7b473dbb3f71139f94ca16bfe5248
waldenlan <446731905@qq.com>
2019-11-25 22:07:57 -0800

    Add the feature to customize the resources used by the central server.

--- dmarket/services/management/commands/init_dmarket.py
+++ dmarket/services/management/commands/init_dmarket.py
@@ -8,12 +8,16 @@ import getpass
 import socket
 import psutil
 import os
+import subprocess
+import xml.etree.ElementTree as ET
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-email', required=False)
         parser.add_argument('-password', required=False)
+        parser.add_argument('-cpu_cores', required=False)
+        parser.add_argument('-memory_size', required=False)
 
     def handle(self, *args, **options):
         existing_users = User.objects.filter(username='admin')
@@ -35,9 +39,33 @@ class Command(BaseCommand):
 
         self.stdout.write('admin user not exist, creating...')
         adminUser = User.objects.create_user('admin', email, password)
-        self.create_machine(adminUser)
-    
-    def create_machine(self, adminUser):
+        self.create_machine(adminUser, options['cpu_cores'], options['memory_size'])
+
+    def update_yarn_config(self, memory_limit, cpu_cores_limit):
+        subprocess.Popen(['stop-yarn.sh']).wait()
+        # https://docs.python.org/3.7/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
+        # begin
+        yarn_config_path = os.path.join(os.environ['HADOOP_CONF_DIR'], 'yarn-site.xml')
+        yarn_config = ET.parse(yarn_config_path)
+        root = yarn_config.getroot()
+        # [TBD] Check if the memory & cpu limit values have already existed
+        memory_config = ET.Element('property')
+        memory_config_name = ET.SubElement(memory_config, 'name')
+        memory_config_name.text = 'yarn.nodemanager.resource.memory-mb'
+        memory_config_value = ET.SubElement(memory_config, 'value')
+        memory_config_value.text = str(memory_limit)
+        root.append(memory_config)
+        cpu_config = ET.Element('property')
+        cpu_config_name = ET.SubElement(cpu_config, 'name')
+        cpu_config_name.text = 'yarn.nodemanager.resource.cpu-vcores'
+        cpu_config_value = ET.SubElement(cpu_config, 'value')
+        cpu_config_value.text = str(cpu_cores_limit)
+        root.append(cpu_config)
+        yarn_config.write(yarn_config_path)
+        # end
+        subprocess.Popen(['start-yarn.sh']).wait()
+
+    def create_machine(self, adminUser, cpu_cores, memory_size):
         # The ways to access the machine data is cited from
         # https://www.pythoncircle.com/post/535/python-script-9-getting-system-information-in-linux-using-python-script/
         # https://stackoverflow.com/questions/1006289/how-to-find-out-the-number-of-cpus-using-python
@@ -48,9 +76,19 @@ class Command(BaseCommand):
             # Could not happen
             raise
 
-        core_num = os.cpu_count()
-        memory_size = psutil.virtual_memory().total
-        # The way to get the ip address of the user machine is cited from https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+        core_limit = os.cpu_count()
+        if cpu_cores is None:
+            cpu_cores = core_limit
+        else:
+            assert 1 <= cpu_cores <= core_limit
+        memory_limit = psutil.virtual_memory().total
+        if memory_size is None:
+            memory_size = memory_limit//1024
+        else:
+            assert 1024 <= memory_size <= memory_limit//1024
+        self.update_yarn_config(memory_size, cpu_cores)
+        # The way to get the ip address of the user machine is cited from
+        # https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
         # begin
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         my_socket.connect(("8.8.8.8", 80))
@@ -59,7 +97,7 @@ class Command(BaseCommand):
         # end
         public_key = File(open(os.path.join(settings.MEDIA_ROOT, Constants.MASTER_PUBKEY_PATH)))
         public_key.name = Constants.MASTER_PUBKEY_PATH
-        master = Machine(ip_address=ip_address, memory_size=memory_size, core_num=core_num,
+        master = Machine(ip_address=ip_address, memory_size=memory_size, core_num=cpu_cores,
                         time_period=Constants.MASTER_AVAILABLE_TIME, user=adminUser,
                         public_key=public_key, hostname=Constants.MASTER_HOST,premium_rate=1)
         master.save()
